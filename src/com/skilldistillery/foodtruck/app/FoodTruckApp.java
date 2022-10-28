54e1efd004f473fc66fadbc4050e6be112a6a9c2
Lowell Belany <lrbelany@gmail.com>
2022-10-28 15:29:35 -0700

    ititialized part of FoodTruck

---
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -0,0 +1,11 @@
+package com.skilldistillery.foodtruck.app;
+
+public class FoodTruckApp {
+
+	public static void main(String[] args) {
+		
+	}
+menues
+scanner 1
+
+}
---
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -0,0 +1,8 @@
+package com.skilldistillery.foodtruck.entities;
+
+public class FoodTruck {
+
+	private static int nextTruckId;
+	private String name;
+    private int id;
+}
